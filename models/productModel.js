d18080b2163114c9eeb02a82f3d6fdff3b640a69
Abhishek Kumar Jha <87441996+AbhishekKumarJha3098@users.noreply.github.com>
2023-04-25 14:54:24 +0530

    Add files via upload

---
+++ models/productModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter a product name"]
+        },
+
+        email: {
+            type: Email,
+            required: [true, "Please enter a product name"]
+        },
+
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: false,
+        },
+
+
+        quality: {
+            type: String,
+            required: false
+             }
+
+            },
+
+   
+
+    {
+        timestamps: true
+    }
+)
+
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
